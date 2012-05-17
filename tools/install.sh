e81d3f65e8d88967dcf2f4563c5bfb2006cad067
Ahir Reddy <ahirreddy@gmail.com>
2012-05-17 01:40:04 -0700

    made 1 more fix

--- tools/install.sh
+++ tools/install.sh
@@ -5,13 +5,7 @@ then
 fi
 
 echo "\033[0;34mCloning Oh My Zsh...\033[0m"
-which git > /dev/null
-if [[ $? -eq 0 ]]; then
-  /usr/bin/env git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-else
-  echo "git not installed"
-  exit
-fi
+/usr/bin/env git clone https://github.com/ahirreddy/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "\033[0;34mLooking for an existing zsh config...\033[0m"
 if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
