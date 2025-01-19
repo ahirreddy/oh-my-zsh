5b93676c6a2420ace2d8bac000676f1dead59893
Ahir Reddy <ahirreddy@gmail.com>
2025-01-19 14:02:27 -0800

    works

--- wezterm.lua
+++ wezterm.lua
@@ -75,6 +75,11 @@ config.colors = {
     "#93A1A1", -- Bright Cyan (Ansi 14)
     "#FDF6E3", -- Bright White (Ansi 15)
   },
+
+  tab_bar = {
+    -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#575757',
+  },
 }
 
 -- Window settings
@@ -95,6 +100,28 @@ config.scrollback_lines = 10000
 
 -- Other settings
 config.enable_tab_bar = true
+
+config.window_frame = {
+  -- The font used in the tab bar.
+  -- Roboto Bold is the default; this font is bundled
+  -- with wezterm.
+  -- Whatever font is selected here, it will have the
+  -- main font setting appended to it to pick up any
+  -- fallback fonts you may have used there.
+  font = wezterm.font { family = 'Roboto', weight = 'Bold' },
+
+  -- The size of the font in the tab bar.
+  -- Default to 10.0 on Windows but 12.0 on other systems
+  font_size = 12.0,
+
+  -- The overall background color of the tab bar when
+  -- the window is focused
+  active_titlebar_bg = '#333333',
+
+  -- The overall background color of the tab bar when
+  -- the window is not focused
+  inactive_titlebar_bg = '#333333',
+}
 
 -- and finally, return the configuration to wezterm
 return config
